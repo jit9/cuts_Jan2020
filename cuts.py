72fede57b285531029d31c25d6146828799d7095
Yilun Guan <zoom.aaron@gmail.com>
2019-01-10 22:01:44 -0500

    Add AnalyzeScan routine (WIP)

--- cuts.py
+++ cuts.py
@@ -1,11 +1,11 @@
 import os
-import numpy as np
-
-from todloop import Routine
+import np as np
+import scipy.stats.mstats as ms
 
 import moby2
 from moby2.scripting import products
 from moby2.analysis import hwp
+from todloop import Routine
 
 
 class CutSources(Routine):
@@ -31,20 +31,18 @@ class CutSources(Routine):
 
         # check if source cut results exist
         sourceResult = os.path.exists(
-            self._depot.get_full_path(moby2.TODCuts,
-                                      tag=self._tag_source, tod=tod) ) 
+            self._depot.get_full_path(
+                moby2.TODCuts, tag=self._tag_source, tod=tod))
         # if cuts exist, load it now
         if sourceResult:
-            self.logger.trace(0, "Loading time stream cuts (%s)" %
-                              self._tag_source)
+            self.logger.trace(
+                0, "Loading time stream cuts (%s)" % self._tag_source)
 
             # load source cut
-            source_cuts = self._depot.read_object(moby2.TODCuts,
-                                                  tag=self._tag_source,
-                                                  tod=tod)
+            source_cuts = self._depot.read_object(
+                moby2.TODCuts, tag=self._tag_source, tod=tod)
             # fill the cuts in the TOD
-            moby2.tod.fill_cuts(tod, source_cuts,
-                                no_noise=self._no_noise)
+            moby2.tod.fill_cuts(tod, source_cuts, no_noise=self._no_noise)
 
         # if source cut cannot be retrieved by tag_source, load it
         # through _source_list
@@ -62,22 +60,21 @@ class CutSources(Routine):
             # find sources that fall in the given TOD
             matched_sources = moby2.ephem.get_sources_in_patch(
                 tod=tod, source_list=source_list)
-            
+
             # check if shift is needed
             if self._shift_params is not None:
                 # calculate pointing offset
-                offset = products.get_pointing_offset(self._shift_params,
-                                                      tod=tod,
-                                                      source_offset=True)
+                offset = products.get_pointing_offset(
+                    self._shift_params, tod=tod, source_offset=True)
 
                 # check if offset is calculated successfully, if not give
                 # a zero offset
                 if offset is None:
-                    offset = (0.,0.)
+                    offset = (0., 0.)
 
                 # calculate a map size
-                if max(offset) > 20./60:
-                    self._mask_params['map_size'] = max(offset) + 10./60
+                if max(offset) > 20. / 60:
+                    self._mask_params['map_size'] = max(offset) + 10. / 60
                 self._mask_params['offset'] = offset
 
             self.logger.info("matched sources: %s" % matched_sources)
@@ -88,9 +85,8 @@ class CutSources(Routine):
             # process source cut for each source
             for source in matched_sources:
                 # compute the source cut associated with the source
-                source_cut = moby2.tod.get_source_cuts( tod, source[1],
-                                                        source[2],
-                                                        **self._mask_params)
+                source_cut = moby2.tod.get_source_cuts(
+                    tod, source[1], source[2], **self._mask_params)
                 # merge the source cut to the total cuts
                 pos_cuts_sources.merge_tod_cuts(source_cut)
 
@@ -125,40 +121,41 @@ class CutPlanets(Routine):
         tod = store.get(self._input_key)
 
         # check if planetCuts exist
-        planetResult = os.path.exists( self._depot.get_full_path(moby2.TODCuts,
-                                                                 tag=self._tag_planet,
-                                                                 tod=tod) ) 
+        planetResult = os.path.exists(
+            self._depot.get_full_path(
+                moby2.TODCuts, tag=self._tag_planet, tod=tod))
         # if planetCuts exist load it into variable pos_cuts_planets
         if planetResult:
-            self.logger.trace(0, "Loading time stream cuts (%s)" % self._tag_planet)
-            pos_cuts_planets = self._depot.read_object(moby2.TODCuts,
-                                                       tag=self._tag_planet, tod=tod)
+            self.logger.trace(
+                0, "Loading time stream cuts (%s)" % self._tag_planet)
+            pos_cuts_planets = self._depot.read_object(
+                moby2.TODCuts, tag=self._tag_planet, tod=tod)
 
         # if planetCuts do not exist generate it on the run
         else:
             logger.trace(0, "Finding new planet cuts")
-            if not hasattr(tod,'fplane'):
-                tod.fplane = products.get_focal_plane(self._pointing_par, tod.info)
+            if not hasattr(tod, 'fplane'):
+                tod.fplane = products.get_focal_plane(self._pointing_par,
+                                                      tod.info)
 
             # load planet sources
-            matched_sources = moby2.ephem.get_sources_in_patch( tod=tod,
-                                                                source_list=None)
+            matched_sources = moby2.ephem.get_sources_in_patch(
+                tod=tod, source_list=None)
 
             # check if shift is needed
             if self._shift_params is not None:
                 # calculate pointing offset
-                offset = products.get_pointing_offset(self._shift_params,
-                                                      tod=tod,
-                                                      source_offset=True)
+                offset = products.get_pointing_offset(
+                    self._shift_params, tod=tod, source_offset=True)
 
                 # check if offset is calculated successfully, if not give
                 # a zero offset
                 if offset is None:
-                    offset = (0.,0.)
+                    offset = (0., 0.)
 
                 # calculate a map size
-                if max(offset) > 20./60:
-                    self._mask_params['map_size'] = max(offset) + 10./60
+                if max(offset) > 20. / 60:
+                    self._mask_params['map_size'] = max(offset) + 10. / 60
                 self._mask_params['offset'] = offset
 
             self.logger.info("matched sources: %s" % matched_sources)
@@ -170,9 +167,8 @@ class CutPlanets(Routine):
             for source in matched_sources:
 
                 # calculate planet cut
-                planet_cut = moby2.tod.get_source_cuts( tod, source[1],
-                                                        source[2],
-                                                        **self._mask_params)
+                planet_cut = moby2.tod.get_source_cuts(
+                    tod, source[1], source[2], **self._mask_params)
                 # merge it into the total cut
                 pos_cuts_planets.merge_tod_cuts(planet_cut)
 
@@ -197,7 +193,7 @@ class RemoveSyncPickup(Routine):
         self._force_sync = params.get('force_sync', False)
         self._tag_sync = params.get('tag_sync', None)
         self._depot_path = params.get('depot', None)
-        
+
     def initialize(self):
         self._depot = moby2.util.Depot(self._depot_path)
 
@@ -206,13 +202,13 @@ class RemoveSyncPickup(Routine):
         tod = store.get(self._input_key)
 
         # Check for existing results, to set what operations must be done/redone.
-        sync_result = os.path.exists( self._depot.get_full_path(moby2.tod.Sync,
-                                                                tag=self._tag_sync,
-                                                                tod=tod))
+        sync_result = os.path.exists(
+            self._depot.get_full_path(
+                moby2.tod.Sync, tag=self._tag_sync, tod=tod))
 
         # determine if sync is needed
-        skip_sync = not self._remove_sync or ( not self._force_sync and
-                                               sync_result )
+        skip_sync = not self._remove_sync or (not self._force_sync
+                                              and sync_result)
 
         # obtain scan frequency
         scan_freq = moby2.tod.get_scan_info(tod).scan_freq
@@ -222,8 +218,8 @@ class RemoveSyncPickup(Routine):
         # check if sync can be skipped
         if skip_sync:
             self.logger.trace(2, "Using old sync")
-            ss = self._depot.read_object(moby2.tod.Sync,
-                                         tag=self._tag_sync, tod=tod)
+            ss = self._depot.read_object(
+                moby2.tod.Sync, tag=self._tag_sync, tod=tod)
         # if not generate it on the go
         else:
             self.logger.trace(2, "Computing new sync")
@@ -236,7 +232,7 @@ class RemoveSyncPickup(Routine):
             #                    force=True)
 
         ss.removeAll()
-        del ss        
+        del ss
 
         # pass the processed tod back to data store
         store.set(self._output_key, tod)
@@ -252,9 +248,9 @@ class CutPartial(Routine):
         self._skip_sync = params.get('skip_sync', False)
         self._glitchp = params.get('glitchp', {})
         self._include_mce = params.get('include_mce', True)
-        self._depot_path = params.get('depot', None)        
+        self._depot_path = params.get('depot', None)
         self._no_noise = params.get('no_noise', True)
-        
+
     def initialize(self):
         self._depot = moby2.util.Depot(self._depot_path)
 
@@ -263,26 +259,24 @@ class CutPartial(Routine):
         tod = store.get(self._input_key)
 
         # check if partial results already exist
-        partial_result = os.path.exists( depot.get_full_path(moby2.TODCuts,
-                                                             tag=self._tag_partial,
-                                                             tod=tod))
+        partial_result = os.path.exists(
+            depot.get_full_path(moby2.TODCuts, tag=self._tag_partial, tod=tod))
         # check if we need to skip creating partial cuts
         skip_partial = self._skip_sync and not self._force_partial and partial_result
 
         # if we want to skip creating partial cuts, load from depot
         if skip_partial:
             # Read existing result
-            cuts_partial = self._depot.read_object(moby2.TODCuts,
-                                                   tag=self._tag_partial,
-                                                   tod=tod)
+            cuts_partial = self._depot.read_object(
+                moby2.TODCuts, tag=self._tag_partial, tod=tod)
         # otherwise generate partial cuts now
         else:
             self.logger.info('Generating partial cuts')
-            
+
             # Generate and save new glitch cuts (note calbol may not be implemented...)
-            cuts_partial = moby2.tod.get_glitch_cuts(tod=tod, params=self._glitchp)
-            
-        
+            cuts_partial = moby2.tod.get_glitch_cuts(
+                tod=tod, params=self._glitchp)
+
         # check if we want to include mce_cuts
         if self._include_mce:
             # find mce cuts
@@ -297,7 +291,8 @@ class CutPartial(Routine):
             #                    force=True)
 
         # fill the partial cuts in our tod
-        moby2.tod.fill_cuts(tod, cuts_partial, extrapolate = False, no_noise=self._no_noise)
+        moby2.tod.fill_cuts(
+            tod, cuts_partial, extrapolate=False, no_noise=self._no_noise)
 
         # save the partial cuts in tod object for further processing
         tod.cuts = cuts_partial
@@ -327,17 +322,18 @@ class SubstractHWP(Routine):
         # retrieve hwp_modes object from depot
         hwp_modes = self._depot.read_object(
             hwp.HWPModes,
-            tag = self._hwp_par['a_chi']['tag'],
+            tag=self._hwp_par['a_chi']['tag'],
             tod=tod,
-            structure= self._hwp_par['a_chi']['structure'] )
+            structure=self._hwp_par['a_chi']['structure'])
 
         # get hwp angles
-        hwp_angles = moby2.scripting.products.get_hwp_angles(self._hwp_par['angles'], tod)
+        hwp_angles = moby2.scripting.products.get_hwp_angles(
+            self._hwp_par['angles'], tod)
 
         # substracting the hwp sinal
-        r = hwp_modes.get_reconstructor(hwp_angles*np.pi/180)
+        r = hwp_modes.get_reconstructor(hwp_angles * np.pi / 180)
         hwp_signal = r.get_achi()
-        tod.data[hwp_modes.det_uid,:]-= hwp_signal
+        tod.data[hwp_modes.det_uid, :] -= hwp_signal
 
         # pass the tod to the data store
         store.set(self._output_key, tod)
@@ -377,3 +373,142 @@ class TransformTOD(Routine):
         if self._n_downsample is not None:
             tod = tod.copy(resample=2**self._n_downsample, resample_offset=1)
             self.logger.trace(0, "Downsampling done")
+
+
+class AnalyzeScan(Routine):
+    def __init__(self, input_key, scanParams):
+        """This routine analyzes the scan pattern"""
+        Routine.__init__(self)
+        self._input_key = input_key
+        self._scanParams = scanParams
+
+    def execute(self, store):
+        # load tod
+        tod = store.get(self._input_key)
+
+        # analyze scan and save result into a dictionary
+        scan = self.analyze_scan(
+            np.unwrap(self.tod.az), self.sampleTime,
+            **self.params.get("scanParams", {}))
+
+        # get scan frequency
+        scan_freq = scan["scan_freq"]
+
+        # get downsample level
+        ds = self.tod.info.downsample_level,
+
+        chunkParams = {
+            'T': scan["T"] * ds,
+            'pivot': scan["pivot"] * ds,
+            'N': scan["N"]
+        }
+
+        store.set("chunkParams", chunkParams)
+
+    def analyze_scan(self, az, dt=0.002508, N=50, vlim=0.01, qlim=0.01):
+        """Find scan parameters and cuts"""
+
+        # Find no motion
+
+        # compute the 1% and 99% quantiles
+        lo, hi = ms.mquantiles(az, (qlim, 1 - qlim))
+
+        # compute the scan speed
+
+        # compute the az steps
+        daz = np.diff(az)
+
+        # form the scan speed array
+        # note that:
+        # daz[0] = az_1 - az_0
+        # daz[1] = az_2 - az_1
+        # 2*daz[0] - daz[1] = 2 az_1 - 2 az_0 - az_2 + az_1
+        #                   = 3 az_1 - 2 az_0 - az_2
+        # this is just an estimate of the scan speed at t=0
+        v_scan = np.r_[2 * daz[0] - daz[1], daz]
+
+        # smooth the scan speed vector with a simple moving average of
+        # length N, the last indexing is to ensure that the size of
+        # the array is the same as v_scan
+        v_smooth = np.convolve(v_scan,
+                               np.ones(N) / N)[(N - 1) / 2:-(N - 1) / 2]
+
+        # estimate the speed using the median of the scan speeds
+        speed = np.median(abs(v_smooth))
+
+        # identify when the scanwhen the speed is either too fast
+        # or two slow. The minimum speed requirement is specified
+        # as a fraction of the median speed using ~vlim~
+        stop = abs(v_scan) < vlim * speed
+        pick = abs(v_smooth) > 2 * speed
+
+        # exit now in case of a stare TOD
+        # i doubt that this is ever going to occur
+        if all(stop):
+            scan = {
+                "az_max": hi,
+                "az_min": lo,
+                "az_speed": speed,
+                "scan_freq": 0.0,
+                "az_cuts": None,
+                "T": len(az),
+                "pivot": 0,
+                "N": 1
+            }
+            return scan
+
+        # Find sections with no scanning by identifying
+        # when the speed is below threshold and when the
+        # scan range is an outlier
+        noscan = stop * (az > lo) * (az < hi)
+
+        # Get scan frequency
+        # first calculate the fourior transform
+        faz = np.fft.rfft(az - az.mean())
+        # identify the highest frequency which corresponds to the
+        # scan frequency
+        fscan = np.where(abs(faz) == abs(faz).max())[0][0] / dt / len(az)
+
+        # Find turnarounds
+        az_min = np.median(az[stop * (az < lo)])
+        az_max = np.median(az[stop * (az > hi)])
+        # i don't understand this part
+        td = abs(lo - az_min) + abs(az_max - hi)
+
+        # Find scan period parameters
+        T_scan = int(1. / fscan / dt)  # number of samples in scan period
+
+        # this is kind of arbitrary
+        T_ex = int(1.2 * T_scan)
+
+        # find non-stopping part of scan
+        onescan = az[~noscan][:T_ex]
+        az_min0 = np.min(onescan[stop[~noscan][:T_ex] * (onescan < lo)
+                                 * (onescan > lo - td)])
+        az_max0 = np.max(onescan[stop[~noscan][:T_ex] * (onescan > hi)
+                                 * (onescan < hi + td)])
+        imin0 = np.where(az == az_min0)[0][0]
+        imax0 = np.where(az == az_max0)[0][0]
+        pivot = np.min([imin0, imax0])  # Index of first scan minima or maxima
+        N_scan = (len(az) - pivot) / T_scan  # Number of complete scan periods
+
+        # Find cuts
+        if hi - lo < 1. * np.pi / 180:
+            flag = np.ones_like(az, dtype=bool)
+        else:
+            flag = (pick + stop) * (az > lo) * (az < hi) + (az < lo - td) + (
+                az > hi + td)
+
+        c_vect = moby2.tod.cuts.CutsVector.from_mask(flag).get_buffered(100)
+
+        scan = {
+            "az_max": az_max,
+            "az_min": az_min,
+            "az_speed": speed,
+            "scan_freq": fscan,
+            "az_cuts": c_vect,
+            "T": T_scan,
+            "pivot": pivot,
+            "N": N_scan
+        }
+        return scan
